4091dc5d9c82a9a5d705d7f552f3f77bbff774c1
Nacho Cabanes <nacho@nachocabanes.com>
2019-10-31 22:50:14 +0100

    143 - Var example

---
+++ chapter03-dataTypes/143-VarExample.cs
@@ -0,0 +1,15 @@
+using System;
+
+class VarExample
+{
+    static void Main()
+    {
+        var a = 5;
+        var b = 5.7f;
+        var c = true;
+        var d = 'f';
+        var e = "Hola";
+        var f = 5.6;
+        var g = 5l;        
+    }    
+}
