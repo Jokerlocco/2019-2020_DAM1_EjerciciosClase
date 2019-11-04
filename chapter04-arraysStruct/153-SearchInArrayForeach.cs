b25628110e4a510ad461215d44cff46663b2100a
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-11-04 17:51:30 +0100

    153 - Search in array, foreach

---
+++ chapter04-arraysStruct/153-SearchInArrayForeach.cs
@@ -0,0 +1,29 @@
+using System;
+
+class SearchInArrayForeach
+{
+    static void Main(string[] args)
+    {
+        const int MAX = 5;
+        long[] data = new long[MAX];
+        long search;
+
+        for (int i = 0; i < MAX; i++)
+        {
+            Console.Write("Enter data " + (i + 1) + ": ");
+            data[i] = Convert.ToInt64(Console.ReadLine());
+        }
+
+        Console.Write("Enter data to search: ");
+        search = Convert.ToInt64(Console.ReadLine());
+
+        bool found = false;
+        foreach (long d in data)
+        {
+            if (d == search)
+                found = true;
+        }
+
+        Console.WriteLine(found);
+    }
+}
