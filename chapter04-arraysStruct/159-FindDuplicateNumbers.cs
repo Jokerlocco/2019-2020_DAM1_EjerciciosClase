9a0eab790f31150d20a69eeb7ae294ea6be0674f
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-11-06 19:50:41 +0100

    159 - Find duplicate numbers

---
+++ chapter04-arraysStruct/159-FindDuplicateNumbers.cs
@@ -0,0 +1,29 @@
+// Finding duplicate numbers in a space-separated string
+
+using System;
+
+class FindDuplicateNumber
+{
+    public static void Main()
+    {
+        Console.Write("Enter a few numbers: ");
+        string[] sentence = Console.ReadLine().Split();
+
+        bool repetitionsFound = false;
+
+        for(int i = 0; i < sentence.Length; i++)
+        {
+            for(int j = i + 1; j < sentence.Length; j++)
+            {
+                if(sentence[i] == sentence[j])
+                {
+                    repetitionsFound = true;
+                    Console.Write(sentence[i] + " ");
+                }
+            }
+        }
+
+        if(! repetitionsFound)
+            Console.WriteLine("There are no repeated words");
+    }
+}
