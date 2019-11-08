566d4815147c3c11f452c2f02fdd184badc7072f
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-11-08 16:32:33 +0100

    167b - Bidimensional array + GetLength

---
+++ chapter04-arraysStruct/167b-ArrayBidim2.cs
@@ -0,0 +1,55 @@
+//Pablo Conde
+//Array bidimensional
+
+using System;
+
+class ArrayBi2
+{
+    static void Main()
+    {
+        int ROWS = 2, COLUMNS = 3;
+        double[,] data = new double[ROWS, COLUMNS];
+
+        // Get data
+
+        for (int row = 0; row < data.GetLength(0); row++)
+        {
+            for (int column = 0; column < data.GetLength(1); column++)
+            {
+                Console.Write("Enter data for pos {0},{1}: ",
+                    row+1, column+1);
+                data[row,column] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+        Console.WriteLine();
+        
+        // Average
+        
+        double sumTotal = 0;
+        for (int row = 0; row < data.GetLength(0); row++)
+        {
+            Console.Write("The average of row {0} is ", row+1);
+            double sum = 0;
+            for (int column = 0; column < data.GetLength(1); column++)
+            {
+                sum += data[row,column];
+                sumTotal += data[row,column];
+            }
+            Console.WriteLine( sum / COLUMNS );
+
+        }
+        Console.WriteLine("Global average is " +
+            sumTotal / data.Length );
+        
+        
+        // Average, alternate way
+        
+        sumTotal = 0;
+        foreach(double d in data)
+        {
+            sumTotal += d;
+        }
+        Console.WriteLine("Global average (2) is " +
+            sumTotal / (ROWS*COLUMNS) );
+    }
+}
