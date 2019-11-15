4d0230749c601e9aa2be1b17d75a788cc3444dcb
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-11-15 16:16:37 +0100

    184e - Books (v 0.05)

---
+++ chapter04-arraysStruct/184e-Books5.cs
@@ -0,0 +1,122 @@
+// Version history
+// 0.05 - Search
+// 0.04 - Display all; bugfix: count when adding
+// 0.03 - Sort by title
+// 0.02 - Add; sort by year
+// 0.01 - Empty skeleton
+
+using System;
+
+class Books
+{
+    struct book
+    {
+        public string author;
+        public string title;
+        public int year;
+    }
+    static void Main()
+    {
+        const int CAPACITY = 25000;
+        int amount = 0;
+        book[] books = new book[CAPACITY];
+        string option;
+
+        do
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1- Add a new book");
+            Console.WriteLine("2- Display all");
+            Console.WriteLine("3- Search");
+            Console.WriteLine("...");
+            Console.WriteLine("X- Exit");
+            option = Console.ReadLine().ToUpper();
+
+            switch (option)
+            {
+                case "1": // Add
+                    if (amount >= CAPACITY)
+                    {
+                        Console.WriteLine("Database full");
+                    }
+                    else
+                    {
+                        Console.Write("Enter author: ");
+                        books[amount].author = Console.ReadLine();
+                        Console.Write("Enter title: ");
+                        books[amount].title = Console.ReadLine();
+                        Console.Write("Enter year: ");
+                        books[amount].year =
+                            Convert.ToInt32(Console.ReadLine());
+                        amount++;
+                    }
+
+                    for (int i = 0; i < amount - 1; i++)
+                    {
+                        for (int j = i + 1; j < amount; j++)
+                        {
+                            if (books[i].title.ToUpper().
+                                CompareTo(books[j].title.ToUpper()) > 0)
+                            {
+                                book aux = books[i];
+                                books[i] = books[j];
+                                books[j] = aux;
+                            }
+                        }
+                    }
+                    break;
+
+                case "2": // Show all
+                    if (amount == 0)
+                        Console.WriteLine("No data to display");
+                    else
+                    {
+                        for (int i = 0; i < amount; i++)
+                        {
+                            Console.WriteLine((i+1) + ": " 
+                                + books[i].author + " - "
+                                + books[i].title + " - " 
+                                + books[i].year);
+                            if (i % 21 == 20)
+                                Console.ReadLine(); // Pause after 21 rows
+                        }
+                    }
+                    break;
+
+                case "3": // Search
+                    if (amount == 0)
+                        Console.WriteLine("No data to search in");
+                    else
+                    {
+                        string search = Console.ReadLine();
+                        bool found = false;
+                        for (int i = 0; i < amount; i++)
+                        {
+                            if (books[i].author.ToLower().Contains(
+                                search.ToLower()))
+                            {
+                                Console.WriteLine((i + 1) + ": "
+                                    + books[i].author + " - "
+                                    + books[i].title + " - "
+                                    + books[i].year);
+                                found = true;
+                            }
+                        }
+                        if (!found)
+                            Console.WriteLine("Not found");
+                    }
+                    break;
+                // TO DO ...
+
+                case "X":
+                    Console.WriteLine("Bye!!");
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong option");
+                    break;
+            }
+        }
+        while (option != "X");
+    }
+}
