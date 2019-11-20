dac81e1d5cdd346ceb531c39716701af786a456f
Nacho Cabanes <nacho@nachocabanes.com>
2019-11-20 22:41:07 +0100

    193 - Function SayHelloToRuth

---
+++ chapter05-functions/193-FunctionSayHelloToRuth.cs
@@ -0,0 +1,16 @@
+using System;
+
+class FunctionSayHelloToRuth
+{
+    static void SayHelloToRuth()
+    {
+        Console.WriteLine("Hi, Ruth!!!");
+    }
+    
+    static void Main()
+    {
+        Console.WriteLine("...");
+        SayHelloToRuth();
+        Console.WriteLine("---");
+    }
+}
