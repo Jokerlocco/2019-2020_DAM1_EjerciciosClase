7071cfc28e3aff7d5588f38452ad8ca7d7c9429f
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-11-22 16:30:18 +0100

    204 - Function DisplayCentered

---
+++ chapter05-functions/204-DisplayCentered.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Centered
+{
+    static void DisplayCentered(string text)
+    {
+        int screenWidth = 80;
+        int halfScreen = screenWidth / 2;
+        int spaces = halfScreen - text.Length / 2;
+        for (int i = 0; i < spaces; i++) Console.Write(" ");
+        Console.WriteLine(text);
+    }
+
+    public static void Main()
+    {
+        DisplayCentered("Hello");
+    }
+}
