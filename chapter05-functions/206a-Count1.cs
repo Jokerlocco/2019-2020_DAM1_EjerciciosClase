685326cb37d78f91ce9e965cc0efcea03b2eab73
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-11-25 17:50:50 +0100

    206 - Function Count (letters)

---
+++ chapter05-functions/206a-Count1.cs
@@ -0,0 +1,21 @@
+using System;
+
+class CountLetter
+{
+    static int Count(string text, char c)
+    {
+        int count = 0;
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == c)
+                count++;
+        }
+        return count;
+    }
+
+    public static void Main()
+    {
+        int amount = Count("madagascar", 'a');
+        Console.WriteLine(amount);
+    }
+}
---
+++ chapter05-functions/206b-Count2.cs
@@ -0,0 +1,21 @@
+using System;
+
+class CountLetter2
+{
+    static int Count(string text, char c)
+    {
+        int count = 0;
+        foreach (char l in text)
+        {
+            if (l == c)
+                count++;
+        }
+        return count;
+    }
+
+    public static void Main()
+    {
+        int amount = Count("madagascar", 'a');
+        Console.WriteLine(amount);
+    }
+}
