a7ea5fbd3086961f7836eed50e02657b49ca71c8
Nacho Cabanes <nacho@nachocabanes.com>
2019-11-27 22:38:47 +0100

    216 - Recursive factorial

---
+++ chapter05-functions/216-FactorialRecursive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class RecursiveFactorial
+{
+    static long Factorial(int num)
+    {
+        // Base case
+        if (num == 1)
+            return 1;
+        else
+        // General case
+            return num * Factorial ( num - 1 );
+    }
+
+    static void Main()
+    {
+        Console.WriteLine( Factorial(16) );
+    }
+}
