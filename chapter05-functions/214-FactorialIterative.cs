51d62694e0923a5acbe86c4d89c17ba6ed65bf1f
Nacho Cabanes <nacho@nachocabanes.com>
2019-11-27 22:37:04 +0100

    214 - Iterative factorial

---
+++ chapter05-functions/214-FactorialIterative.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class IterativeFactorial
+{
+    static long Factorial(int num)
+    {
+        long total = 1;
+        
+        for (int i = 1; i <= num; i++)
+        {
+            total *= i;
+        }
+        return total;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine( Factorial(16) );
+    }
+}
