b3490f2c8614b78014b209f8d7b8f33ad702695f
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-12-04 20:25:06 +0100

    240 - DrawParallelogram

---
+++ chapter05-functions/240-DrawParallelogram.cs
@@ -0,0 +1,38 @@
+﻿/*
+DrawParallelogram(10, 4, '*'); =>
+
+**********
+ *        *
+  *        *
+   ********** 
+
+*/
+
+using System;
+
+class Parallelogram
+{
+    static void DrawParallelogram(byte width, byte height, char symbol)
+    {
+        // First line
+        Console.WriteLine(new string (symbol, width));
+
+        // Middle lines
+        string spaces = new string(' ', width - 2);
+        for (int i = 1; i < height-1; i++)
+        {
+            Console.WriteLine(new string (' ', i) + 
+                symbol + spaces + symbol);
+        }
+
+        // Last line
+        Console.WriteLine(new string (' ', height-1) 
+            + new string (symbol, width));
+    }
+
+
+    static void Main(string[] args)
+    {
+        DrawParallelogram(10, 4, '*');
+    }
+}
