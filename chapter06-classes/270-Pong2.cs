a76d5eaf3e8421c467a16aa3262fd9c6247786d0
Nacho Cabanes <nacho@nachocabanes.com>
2019-12-16 23:00:25 +0100

    270 - Empty class Pong + Empty class Game

---
+++ chapter06-classes/270-Pong2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+// --------------------------------------
+
+class Pong
+{
+    public Pong()
+    {
+        Console.WriteLine("Starting Pong...");
+    }
+
+    ~Pong()
+    {
+        Console.WriteLine("Session finished");
+    }
+
+    static void Main()
+    {
+        Pong pong = new Pong();
+        Game g = new Game();
+    }
+}
+
+
+// --------------------------------------
+
+class Game
+{
+    public Game()
+    {
+        Console.WriteLine("Starting game...");
+    }
+}
