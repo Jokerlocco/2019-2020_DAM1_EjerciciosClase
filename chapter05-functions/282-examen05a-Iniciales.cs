bb28260d85b60a0aecf84fd3a2fc5023ad0deb42
Nacho Cabanes <ncabanes@users.noreply.github.com>
2020-01-13 16:42:16 +0100

    282 - Exam of chapter 5a: Initials

---
+++ chapter05-functions/282-examen05a-Iniciales.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Funciones
+{
+    static char[] ObtenerIniciales(string cadena)
+    {
+        string[] palabras = cadena.Split();
+        int tamanyo = palabras.Length;
+        char[] iniciales = new char[tamanyo];
+     
+        for (int p = 0; p < tamanyo; p++)
+        {
+            iniciales[p] = palabras[p][0];
+        }
+        
+        return iniciales;
+    }
+
+    static void Main()
+    {
+        string cadena = "Hola, como estás";
+        char primeraInicial = ObtenerIniciales(cadena)[0];
+
+        Console.WriteLine(primeraInicial);
+    }
+}
