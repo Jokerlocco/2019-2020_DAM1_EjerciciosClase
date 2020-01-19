54881ff153b90b4c8c068f1d14edc9a849a820d5
Nacho Cabanes <nacho@nachocabanes.com>
2020-01-19 11:38:36 +0100

    SpaceHawks MonoGame 009 - Functions for the game loop

--- projects/SpaceHawks/Game1.cs
+++ projects/SpaceHawks/Game1.cs
@@ -35,23 +35,7 @@ namespace SpaceHawks
             graphics.ApplyChanges();
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
 
-            base.Initialize();
-        }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
@@ -72,39 +56,36 @@ namespace SpaceHawks
         }
 
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back 
-                    == ButtonState.Pressed 
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back
+                    == ButtonState.Pressed
                     || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // Player movement
-            var keyboardState = Keyboard.GetState();
-            if (keyboardState.IsKeyDown(Keys.Left))
-                shipPosition.X -= shipSpeed * 
-                    (float)gameTime.ElapsedGameTime.TotalSeconds;
-            if (keyboardState.IsKeyDown(Keys.Right))
-                shipPosition.X += shipSpeed *
-                    (float)gameTime.ElapsedGameTime.TotalSeconds;
+            MovePlayer(gameTime);
+            MoveEnemy(gameTime);
+            CheckCollisions();
 
-            // Enemy movement
+            base.Update(gameTime);
+        }
+
+        private void CheckCollisions()
+        {
+            Rectangle spaceshipRect = new Rectangle(
+                (int)shipPosition.X, (int)shipPosition.Y,
+                spaceship.Width, spaceship.Height);
+            Rectangle enemyRect = new Rectangle(
+                (int)enemyPosition.X, (int)enemyPosition.Y,
+                enemy.Width, enemy.Height);
+            if (spaceshipRect.Intersects(enemyRect))
+                Exit();
+        }
+
+        private void MoveEnemy(GameTime gameTime)
+        {
             enemyPosition.X += enemySpeed.X *
-                (float)gameTime.ElapsedGameTime.TotalSeconds;
+                            (float)gameTime.ElapsedGameTime.TotalSeconds;
             enemyPosition.Y += enemySpeed.Y *
                 (float)gameTime.ElapsedGameTime.TotalSeconds;
 
@@ -112,29 +93,22 @@ namespace SpaceHawks
                 enemySpeed.X = -enemySpeed.X;
             if ((enemyPosition.Y < 20) || (enemyPosition.Y > 400))
                 enemySpeed.Y = -enemySpeed.Y;
-
-            //Collisions checking
-            Rectangle spaceshipRect = new Rectangle(
-                (int)shipPosition.X, (int)shipPosition.Y,
-                spaceship.Width, spaceship.Height);
-            Rectangle enemyRect = new Rectangle(
-                    (int)enemyPosition.X, (int)enemyPosition.Y,
-                    enemy.Width, enemy.Height);
-            if (spaceshipRect.Intersects(enemyRect))
-                Exit();
-
-            // Fire !!!
-            if (keyboardState.IsKeyDown(Keys.Space))
-                fireSound.CreateInstance().Play();
-
-
-            base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected void MovePlayer(GameTime gameTime)
+        {
+            var keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.Left))
+                shipPosition.X -= shipSpeed *
+                    (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (keyboardState.IsKeyDown(Keys.Right))
+                shipPosition.X += shipSpeed *
+                    (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (keyboardState.IsKeyDown(Keys.Space))
+                fireSound.CreateInstance().Play();
+        }
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
