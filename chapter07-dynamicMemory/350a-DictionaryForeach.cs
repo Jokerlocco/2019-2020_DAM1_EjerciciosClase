2e24cac2001beb236f868e4d789a8094ed39d65b
Nacho Cabanes <nacho@nachocabanes.com>
2020-02-14 13:49:46 +0100

    350a - Dictionary + Generic + foreach

---
+++ chapter07-dynamicMemory/350a-DictionaryForeach.cs
@@ -0,0 +1,28 @@
+// Pablo Vigara Fernandez
+
+using System;
+using System.Collections.Generic;
+
+class DictionaryForeach
+{
+    static void Main()
+    {
+        Dictionary<string, string> dic = new Dictionary<string, string>();
+        dic.Add("Hola", "Hello");
+        dic["Adios"] = "Good bye";
+        dic["Hasta luego"] = "See you later";
+        dic["Tarde"] = "Late";
+        dic["Pronto"] = "Soon";
+        dic["Ayer"] = "Yesterday";
+
+        foreach (KeyValuePair<string, string> entry in dic)
+        {
+            Console.WriteLine("{0} --> {1}", entry.Key, entry.Value);
+        }
+        
+        foreach (var entry in dic)
+        {
+            Console.WriteLine("{0} --> {1}", entry.Key, entry.Value);
+        }
+    }
+}
