4a1a6110b2ae1eef160e00a69fb851edbff90e63
Nacho Cabanes <nacho@nachocabanes.com>
2020-02-19 11:46:30 +0100

    MiniMiner 0.03 - First Tilemap

--- projects/miniMiner/PantallaDeBienvenida.cs
+++ projects/miniMiner/PantallaDeBienvenida.cs
@@ -10,6 +10,35 @@ namespace MiniMiner
         private SpriteFont fuente;
         private GestorDePantallas gestor;
 
+        private byte[,] fondo =
+        {
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1}
+        };
+
+        private byte anchoFondo = 20;
+        private byte altoFondo = 16;
+        private short margenXFondo = 200;
+        private byte margenYFondo = 100;
+        private byte anchoCasillaFondo = 32;
+        private byte altoCasillaFondo = 32;
+        private Texture2D pared;
+        private Texture2D suelo;
+
         public PantallaDeBienvenida(GestorDePantallas gestor)
         {
             this.gestor = gestor;
@@ -18,6 +47,8 @@ namespace MiniMiner
         public void CargarContenidos(ContentManager Content)
         {
             fuente = Content.Load<SpriteFont>("Arial");
+            pared = Content.Load<Texture2D>("paredNivel1");
+            suelo = Content.Load<Texture2D>("sueloNivel1");
         }
 
         public void Actualizar(GameTime gameTime)
@@ -34,6 +65,26 @@ namespace MiniMiner
 
         public void Dibujar(GameTime gameTime, SpriteBatch spritebatch)
         {
+            for (int fila = 0; fila < altoFondo; fila++)  // Fondo
+                for (int col = 0; col < anchoFondo; col++)
+                {
+                    if (fondo[fila, col] == 1)
+                        spritebatch.Draw(pared,
+                            new Rectangle(
+                                margenXFondo + col * anchoCasillaFondo,
+                                margenYFondo + fila * altoCasillaFondo,
+                                anchoCasillaFondo,
+                                altoCasillaFondo),
+                            Color.White);
+                    else if (fondo[fila, col] == 2)
+                        spritebatch.Draw(suelo,
+                            new Rectangle(
+                                margenXFondo + col * anchoCasillaFondo,
+                                margenYFondo + fila * altoCasillaFondo,
+                                anchoCasillaFondo,
+                                altoCasillaFondo),
+                            Color.White);
+                }
             spritebatch.DrawString(fuente, "1. Jugar",
                 new Vector2(400, 100), Color.White);
             spritebatch.DrawString(fuente, "S. Salir",
