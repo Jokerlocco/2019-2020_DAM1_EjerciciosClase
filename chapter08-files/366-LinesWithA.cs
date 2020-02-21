a859c202215c02d5ee25985edc312eb6e5b19082
Nacho Cabanes <ncabanes@users.noreply.github.com>
2020-02-21 17:03:44 +0100

    366 - File: Lines starting with A

---
+++ chapter08-files/366-LinesWithA.cs
@@ -0,0 +1,32 @@
+﻿// Gonzalo Arques
+
+using System;
+using System.IO;
+
+class LinesWithA
+{
+    static void Main()
+    {
+        string[] fileData;
+
+        Console.Write("Enter the file name: ");
+        string fileName = Console.ReadLine();
+
+        if ( !File.Exists(fileName) )
+        {
+            Console.WriteLine("File does not exist");
+        }
+        else
+        {
+            fileData = File.ReadAllLines(fileName);
+
+            for (int lineNum = 0; lineNum < fileData.Length; lineNum++)
+            {
+                if (fileData[lineNum].StartsWith("A"))
+                {
+                    Console.WriteLine(fileData[lineNum]);
+                }
+            }
+        }
+    }
+}
