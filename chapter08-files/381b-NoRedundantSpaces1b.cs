08ea19b5bc05d4f8481bca9eaf5ca761576e3732
Nacho Cabanes <ncabanes@users.noreply.github.com>
2020-02-27 15:30:44 +0100

    381b - NoRedundantSpaces (v1b)

---
+++ chapter08-files/381b-NoRedundantSpaces1b.cs
@@ -0,0 +1,38 @@
+// No redundant spaces
+// Version 1a: StreamReader + StreamWriter, OpenText + CreateText
+
+using System;
+using System.IO;
+
+public class NoRedundantSpaces
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 1)
+            return;
+
+        string name = args[0];
+        StreamReader input = new StreamReader(name);
+        StreamWriter output = new StreamWriter( name + ".nospc.txt");
+        
+        string line;
+        int count = 1;
+        do
+        {
+            line = input.ReadLine();
+            if (line != null)
+            {
+                line = line.Trim();
+                while (line.Contains("  "))
+                    line = line.Replace("  ", " ");
+                output.WriteLine(count + ". " + line);
+                count++;
+            }
+        }
+        while (line != null);
+        
+        output.Close();
+        input.Close();
+        Console.WriteLine("Redundant spaces removed");
+    }
+}
