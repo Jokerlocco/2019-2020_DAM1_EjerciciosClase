9ef8969e22540b62fa79659ae41fac78d9fd3ace
Nacho Cabanes <ncabanes@users.noreply.github.com>
2020-03-02 16:23:33 +0100

    391 - Count spaces (v1)

---
+++ chapter08-files/391-CountSpaces1.cs
@@ -0,0 +1,32 @@
+﻿// Count spaces (V1)
+// Pablo Vigara Fernandez
+
+using System;
+using System.IO;
+
+class CountSpaces1
+{
+    static void Main()
+    {
+        Console.Write("Enter file name: ");
+        string path = Console.ReadLine();
+
+        FileStream fich = File.OpenRead(path);
+        
+        int fichByte;
+        int count = 0;
+
+        do
+        {
+            fichByte = fich.ReadByte();
+
+            if (fichByte != -1)
+                if (fichByte == ' ')
+                    count++;
+        }
+        while (fichByte != -1);
+        Console.Write("Spaces: " + count);
+
+        fich.Close();
+    }
+}
