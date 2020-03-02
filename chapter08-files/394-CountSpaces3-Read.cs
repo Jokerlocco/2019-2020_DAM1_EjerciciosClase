2fa8c1fdc652e396e9d0e43c7fbcf58a93a3d425
Nacho Cabanes <ncabanes@users.noreply.github.com>
2020-03-02 17:04:09 +0100

    394 - Count spaces V3 (Read, block)

---
+++ chapter08-files/394-CountSpaces3-Read.cs
@@ -0,0 +1,36 @@
+﻿// Pablo Vigara Fernandez
+
+using System;
+using System.IO;
+
+class Ejercicio
+{
+    static void Main()
+    {
+        Console.Write("Nombre del archivo: ");
+        string path = Console.ReadLine();
+
+        try
+        {
+            FileStream fich = new FileStream(path, FileMode.Open);            
+            int cantidadALeer = (int) fich.Length;
+            byte[] datos = new byte[cantidadALeer];
+            int cantidadLeida = fich.Read(datos, 0, cantidadALeer);
+            fich.Close();
+            
+            if (cantidadLeida != cantidadALeer)
+                Console.WriteLine("Read error!");
+
+            int count = 0;
+            for (long i = 0; i < datos.Length; i++)
+            {
+                if (datos[i] == ' ')
+                    count++;
+            }
+            Console.Write("Espacios: " + count);
+
+            
+        }
+        catch (Exception e) { Console.WriteLine("Error: " + e.Message); }
+    }
+}
