54f9db2a95ba65dde72e6eed0890d8dac474332e
Nacho Cabanes <nacho@nachocabanes.com>
2020-03-16 20:26:14 +0100

    413 - Bytes 97 to file

---
+++ chapter08-files/413-Bytes97.cs
@@ -0,0 +1,27 @@
+﻿// Pablo Vigara Fernandez
+
+using System;
+using System.IO;
+
+class Ejercicio3
+{
+    static void Main()
+    {
+        try
+        {
+            FileStream output = File.Create("prueba.txt");
+
+            for (int i = 0; i < 100; i++)
+            {
+                output.WriteByte(97);
+            }
+            output.Close();
+            Console.WriteLine("Fichero prueba.txt creado correctamente");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+}
+ 
