2f043bbf4bb47560096d71e12f89cec97c1ce06f
Nacho Cabanes <nacho@nachocabanes.com>
2020-04-01 21:14:26 +0200

    440 - Add 7 days

---
+++ chapter09-libraries/440-Every7Days.cs
@@ -0,0 +1,24 @@
+﻿// Gonzalo Arques
+
+using System;
+using System.IO;
+
+class Programa
+{
+    static void Main()
+    {
+        StreamWriter fichero = File.CreateText("tasks.txt");
+
+        DateTime tiempoActual = DateTime.Now;
+        DateTime finalAnyo = new DateTime(2020, 12, 31);
+
+        while (tiempoActual <= finalAnyo)
+        {
+            fichero.WriteLine(tiempoActual.ToString("d") + ": ");
+            tiempoActual = tiempoActual.AddDays(7);
+        }
+
+        fichero.Close();
+        Console.WriteLine("El fichero se ha creado correctamente.");
+    }
+}
