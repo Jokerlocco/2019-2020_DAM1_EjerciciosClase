ec36ae3060b8d01557861b41d42c7dc82983e64f
Nacho Cabanes <nacho@nachocabanes.com>
2020-04-03 23:21:25 +0200

    448 - Console Launcher (2 versions)

---
+++ chapter09-libraries/448a-ConsoleLauncher1.cs
@@ -0,0 +1,50 @@
+﻿// Gonzalo Arques
+
+using System;
+using System.Diagnostics;
+
+class Programa
+{
+    static void Main()
+    {
+        byte opcionElegida;
+        string[] listaPrograma = { "notepad.exe", "Code.exe", "msedge.exe",
+            "devenv.exe", "chrome.exe", "ONENOTE.EXE", "WINWORD.EXE",
+            "Steam.exe", "mspaint.exe", "GeanyPortable.exe" };
+        
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("0- Notepad");
+            Console.WriteLine("1- Visual Studio Code");
+            Console.WriteLine("2- Microsoft Edge");
+            Console.WriteLine("3- Visual Studio Community");
+            Console.WriteLine("4- Chrome");
+            Console.WriteLine("5- OneNote");
+            Console.WriteLine("6- Word");
+            Console.WriteLine("7- Steam");
+            Console.WriteLine("8- Paint");
+            Console.WriteLine("9- Geany");
+            Console.WriteLine();Console.WriteLine("10- Salir");
+            Console.WriteLine();Console.Write("Elige un programa para abrir: ");
+            opcionElegida = Convert.ToByte(Console.ReadLine());
+
+            if (opcionElegida != 10)
+            {
+                Console.WriteLine("Lanzando el programa deseado...");
+                
+                try
+                {
+                    Process procesoPrograma = Process.Start(listaPrograma[opcionElegida]);
+                    procesoPrograma.WaitForExit();
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("No se ha podido lanzar ese programa");
+                    Console.ReadLine();
+                }
+            }
+        }
+        while (opcionElegida != 10);
+    }
+}
---
+++ chapter09-libraries/448b-ConsoleLauncher2.cs
@@ -0,0 +1,47 @@
+﻿// Gonzalo Arques, improved by Nacho
+
+using System;
+using System.Diagnostics;
+
+class Programa
+{
+    static void Main()
+    {
+        byte opcionElegida;
+        string[] nombres = { "Bloc de Notas", "Visual Studio Code", 
+            "Microsoft Edge", "Visual Studio Community", "Chrome", 
+            "OneNote", "Word", "Steam", "Paint", "Geany" };
+        string[] ejecutables = { "notepad.exe", "Code.exe", 
+            "msedge.exe",
+            "devenv.exe", "chrome.exe", "ONENOTE.EXE", "WINWORD.EXE",
+            "Steam.exe", "mspaint.exe", "GeanyPortable.exe" };
+        
+        do
+        {
+            Console.Clear();
+            for (int i = 0; i < nombres.Length; i++)
+                Console.WriteLine( i + "- " + nombres[i]);
+
+            Console.WriteLine();Console.WriteLine(nombres.Length+"- Salir");
+            Console.WriteLine();Console.Write("Elige un programa para abrir: ");
+            opcionElegida = Convert.ToByte(Console.ReadLine());
+
+            if (opcionElegida != nombres.Length)
+            {
+                Console.WriteLine("Lanzando el programa deseado...");
+                
+                try
+                {
+                    Process procesoPrograma = Process.Start(ejecutables[opcionElegida]);
+                    procesoPrograma.WaitForExit();
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("No se ha podido lanzar ese programa");
+                    Console.ReadLine();
+                }
+            }
+        }
+        while (opcionElegida != nombres.Length);
+    }
+}
